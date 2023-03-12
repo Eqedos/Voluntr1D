827c953a6a502c77b6772dfb3326c159bf45467c
Parth <parthkumarj117@@gmail.com>
2023-03-12 17:55:32 +0800

    Fixed Login Bugs

--- app/src/main/java/com/example/voluntr/LoginActivity.java
+++ app/src/main/java/com/example/voluntr/LoginActivity.java
@@ -64,11 +64,16 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        onStart();{
-            mAuth.addAuthStateListener(firebaseAuthStateListener);
-        };
-        onStop();{
-            mAuth.removeAuthStateListener(firebaseAuthStateListener);
-        }
     };
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
 }
