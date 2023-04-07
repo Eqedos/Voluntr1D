1b07cf0804cf0a015ef58712a0db4abb853b4fa0
vanyajalan <vanyajalan02@gmail.com>
2023-04-08 03:13:22 +0800

    ViewPager added

---
+++ app/src/main/java/com/example/voluntr/ViewPagerActivity.java
@@ -0,0 +1,83 @@
+package com.example.voluntr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class ViewPagerActivity extends AppCompatActivity {
+    Button skipbtn;
+    ViewPager mSlideViewPager;
+    LinearLayout mDotLayout;
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorialslider);
+        skipbtn = findViewById(R.id.skip);
+        mSlideViewPager =(ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        mSlideViewPager.setAdapter(viewPagerAdapter);
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(ViewPagerActivity.this,LoginRegActivity.class);
+                startActivity(i);
+                finish();
+                return;
+
+
+            }
+        });
+        setUpIndicator(0); //set first dot as active
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+    public void setUpIndicator(int position){ //adding dots to layout tutorial_slider
+        dots=new TextView[4]; //4 dots
+        mDotLayout.removeAllViews();
+        for(int i=0; i< dots.length;i++)
+        {
+            dots[i]=new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226")); //html code to add dots
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.dotinactive));
+            mDotLayout.addView(dots[i]);
+        }
+        dots[position].setTextColor(getResources().getColor(R.color.dotactive));
+    }
+    ViewPager.OnPageChangeListener viewListener =new ViewPager.OnPageChangeListener() { //when user scrolls
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            setUpIndicator(position); //pass position --> whichever position respective color will be active for dot
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+    private int getitem(int i) { //whenever we pass something, slideViewPager gives position of viewPager
+        return mSlideViewPager.getCurrentItem()+i;
+    }
+
+}
---
+++ app/src/main/java/com/example/voluntr/ViewPagerAdapter.java
@@ -0,0 +1,71 @@
+package com.example.voluntr;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Context context;
+    int images[]={ //add image ids in array
+            R.drawable.feature1,
+    R.drawable.feature2,
+    R.drawable.feature3,
+    R.drawable.feature4 };
+    int headings[] ={ //headings in array
+            R.string.feature1,
+            R.string.feature2,
+            R.string.feature3,
+            R.string.feature4 };
+
+
+    int description[] = {   //description of every feature in array
+            R.string.desc1,
+            R.string.desc2,
+            R.string.desc3,
+            R.string.desc4  };
+
+
+    public ViewPagerAdapter(Context context) {
+        this.context=context;
+    }
+    //implement methods
+    @Override
+    public int getCount() { //how many layouts we put in ViewPager
+        return headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @NonNull
+    @Override
+    //called when ViewPager is scrolled
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view= layoutInflater.inflate(R.layout.tutorial_slider, container, false);
+        ImageView slidetitleimage = (ImageView)view.findViewById(R.id.titleImage);
+        TextView slideheading = (TextView) view.findViewById(R.id.text_title);
+        TextView slidedescription = (TextView) view.findViewById(R.id.textdescription);
+        //set image, heading, description according to slide number
+        slidetitleimage.setImageResource(images[position]);
+        slideheading.setText(headings[position]);
+        slidedescription.setText(description[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
